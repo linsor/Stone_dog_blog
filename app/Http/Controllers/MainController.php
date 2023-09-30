dd8ef208313c23376b09e8bb50dcee63a54dd3ad
linsor <kfamilion51@gmail.com>
2023-09-30 16:33:42 +0600

    Test create Route and controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use App\Models\Post;
+
+class MainController extends BaseController
+{
+    public function __invoke() {
+        dd(111111);
+    }
+}
